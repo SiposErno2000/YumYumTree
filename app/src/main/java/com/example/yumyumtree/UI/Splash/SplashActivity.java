328fa881aa857adc2dabc483f61fee84c18a435c
Sipos Ernő <siposerno2000@gmail.com>
2021-01-28 12:09:07 +0200

    created a simple splash screen with a small animation

---
+++ app/src/main/java/com/example/yumyumtree/UI/Splash/SplashActivity.java
@@ -0,0 +1,50 @@
+package com.example.yumyumtree.UI.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.yumyumtree.R;
+import com.example.yumyumtree.UI.Login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 3000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView logo;
+    TextView rest_name, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo = findViewById(R.id.logo);
+        rest_name = findViewById(R.id.rest_name);
+        slogan = findViewById(R.id.slogan);
+
+        logo.setAnimation(topAnimation);
+        rest_name.setAnimation(bottomAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
